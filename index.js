be22c5bc51dd93d5ab6ef48c1627e9bee2372eef
silent-node <98669451+silent-node@users.noreply.github.com>
2022-06-27 19:01:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ client.on("ready", () => {
  console.log("hi");
 });
 
+client.on("guildCreate", async (guild) => {
+  const channel = client.guild.channels.cache.get("977887542789668865")
+  const embed = new MessageEmbed()
+  .setTitle("Joined")
+  .setColor("RANDOM")
+ channel.send(embed);
+
 client.login("OTg5MTE1NjkxNTY1MjExNjg4.GNmDmm.aQAbkZL_dWU-WxbrdHdvR6Rek-zZUeW9irUpek");
